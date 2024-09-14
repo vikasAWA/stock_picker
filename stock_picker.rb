8b9b8e40229d1fc71a4a66ce4d49015bf3eac4cc
vikasAWA <85212839+vikasAWA@users.noreply.github.com>
2024-09-14 18:43:36 +0530

    Used Simpler logic

--- stock_picker.rb
+++ stock_picker.rb
@@ -1,20 +1,16 @@
-def stock_picker(arr)
+def stock_picker(prices)
   max_profit = 0
   pair = nil
-  arr.each_with_index.reduce(arr.dup) do |acc, (buy_stock, buy_idx)|
-    acc.shift
-    acc.each do |sell_stock|
-      profit = sell_stock - buy_stock
-      sell_idx = arr.index(sell_stock)
-      if profit > max_profit
-        max_profit = profit
-        pair = [buy_idx, sell_idx]
+  prices.each_with_index do |buy_price, buy_day|
+    prices.each_with_index do |sell_price, sell_day|
+      if sell_day > buy_day && (sell_price - buy_price) > max_profit 
+        max_profit = sell_price - buy_price
+        pair = [buy_day, sell_day]
       end
-    end
-    acc
+    end 
   end
   pair
-end 
+end
 
 p stock_picker([17,3,6,9,15,8,6,1,10])
 p stock_picker([100,131,111,119,151,118,206,90])
